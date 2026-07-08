03979afb93297a13130fa2dec284767058cffb31
leo771331 <jeo02269@gmail.com>
2026-07-08 16:11:13 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,11 @@
+-- 코드를 작성해주세요
+SELECT DISTINCT
+    d.ID,
+    d.EMAIL,
+    d.FIRST_NAME,
+    d.LAST_NAME
+FROM DEVELOPERS AS d
+JOIN SKILLCODES AS s
+ON d.SKILL_CODE & s.CODE
+WHERE s.NAME IN ('Python', 'C#')
+ORDER BY d.ID ASC; 
