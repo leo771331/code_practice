1d566cdd5778b7ec769af38cef8823c24bc99993
leo771331 <jeo02269@gmail.com>
2026-07-11 16:02:07 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,17 @@
+-- 코드를 입력하세요
+SELECT
+    a.APNT_NO,
+    p.PT_NAME,
+    p.PT_NO,
+    a.MCDP_CD,
+    d.DR_NAME,
+    a.APNT_YMD
+FROM APPOINTMENT a
+JOIN DOCTOR d
+    ON a.MDDR_ID = d.DR_ID
+JOIN PATIENT p
+    ON a.PT_NO = p.PT_NO
+WHERE DATE(a.APNT_YMD) = '2022-04-13'
+    AND a.APNT_CNCL_YN = 'N'
+    AND a.MCDP_CD = 'CS'
+ORDER BY a.APNT_YMD ASC;
