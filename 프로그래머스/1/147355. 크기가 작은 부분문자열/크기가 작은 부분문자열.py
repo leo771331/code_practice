dd3509613e445f2e307a674dad32a8ca0a143518
leo771331 <jeo02269@gmail.com>
2026-07-23 15:47:39 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.30 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -0,0 +1,10 @@
+def solution(t, p):
+    answer = 0
+
+    for i in range(len(t) - len(p) + 1):
+        temp = t[i:i+len(p)]
+
+        if int(temp) <= int(p):
+            answer += 1
+
+    return answer
